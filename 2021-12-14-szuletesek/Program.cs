eb950690341d9ef90e4bb76a287df5d0c438fe90
BariBaso <kissmarci41@gmail.com>
2021-12-14 10:18:32 +0100

    Add project files.

---
+++ 2021-12-14-szuletesek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_12_14_szuletesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
