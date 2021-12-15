eb255e3291df5e7dbda492f0f944b3b7e57d209d
BariBaso <kissmarci41@gmail.com>
2021-12-15 12:25:57 +0100

    3.feladat eleje

--- 2021-12-14-szuletesek/szemely.cs
+++ 2021-12-14-szuletesek/szemely.cs
@@ -9,6 +9,25 @@ namespace _2021_12_14_szuletesek
     class szemely
     {
         public string SzemelyiSzam { get; set; }
+
+        public bool CvdEll { 
+            get
+            {
+                string sz = SzemelyiSzam.Replace("-","");
+                int CheckSum = sz[sz.Length];
+                int a = 10;
+                int ösz =0;
+                for (int i = 0; i < sz.Length-1; i++)
+                {
+                    ösz=+ sz[i] * a;
+                    a--;
+                }
+
+
+                return false;
+            }
+        
+        }
         public szemely(string sor)
         {
             SzemelyiSzam = sor;
