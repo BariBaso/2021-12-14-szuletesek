010d594484c2a33e21c6ed03c9f35727bd0b193a
BariBaso <kissmarci41@gmail.com>
2021-12-17 12:24:41 +0100

    5.-7. feladat

--- 2021-12-14-szuletesek/Program.cs
+++ 2021-12-14-szuletesek/Program.cs
@@ -23,18 +23,39 @@ namespace _2021_12_14_szuletesek
                 .ForEach(sz => Console.WriteLine($"\tHibás az {sz.SzemelyiSzam}szemelyi azonosíto"));
             int törlölt = szemelyek.RemoveAll(x => !x.CvdEll);
 
-          /*  Console.WriteLine(szemelyek.Count);
-            for (int i = 0; i < szemelyek.Count; i++)
-            {
-                if (!szemelyek[i].CvdEll)
-                {
-                    Console.WriteLine($"\tHibás az {szemelyek[i].SzemelyiSzam}szemelyi azonosíto");
-                    szemelyek.Remove(szemelyek[i]);
-                }
+            /*  Console.WriteLine(szemelyek.Count);
+              for (int i = 0; i < szemelyek.Count; i++)
+              {
+                  if (!szemelyek[i].CvdEll)
+                  {
+                      Console.WriteLine($"\tHibás az {szemelyek[i].SzemelyiSzam}szemelyi azonosíto");
+                      szemelyek.Remove(szemelyek[i]);
+                  }
 
-               
+
+              }
+              Console.WriteLine(szemelyek.Count);*/
+
+            Console.WriteLine($"5. feladat vaz megyében a vicsgált évek alatt {szemelyek.Count} csecsemő született");
+
+            int fiuk = szemelyek.Where(x => x.SzemelyiSzam.First() == '1' || x.SzemelyiSzam.First() == '3').ToList().Count();
+            Console.WriteLine($"Fiuk száma {fiuk}");
+
+            int minEv = 0, maxEv = 0;
+
+            foreach (var sz in szemelyek)
+            {
+                int AK = int.Parse(sz.SzemelyiSzam.Substring(2, 2));
+                if (AK >= 97 && AK <= 99)
+                    AK += 1900;
+                else
+                    AK += 2000;
+                if (AK < minEv) minEv = AK;
+
+                if (AK > maxEv) maxEv = AK;
             }
-            Console.WriteLine(szemelyek.Count);*/
+            Console.WriteLine(minEv);
+            Console.WriteLine(maxEv);
 
             Console.ReadKey();
            
