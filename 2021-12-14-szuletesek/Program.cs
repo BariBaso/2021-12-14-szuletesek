f1ff6147c178c6a1e86f2b332d9d3b1bb9eb7c8b
BariBaso <kissmarci41@gmail.com>
2021-12-17 11:29:32 +0100

    4. feladat

--- 2021-12-14-szuletesek/Program.cs
+++ 2021-12-14-szuletesek/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _2021_12_14_szuletesek
 {
@@ -15,7 +16,28 @@ namespace _2021_12_14_szuletesek
                 szemelyek.Add(new szemely(sor));
             }
 
-        
+            Console.WriteLine("ellenörzés");
+
+            szemelyek
+                .Where(x => !x.CvdEll).ToList()
+                .ForEach(sz => Console.WriteLine($"\tHibás az {sz.SzemelyiSzam}szemelyi azonosíto"));
+            int törlölt = szemelyek.RemoveAll(x => !x.CvdEll);
+
+          /*  Console.WriteLine(szemelyek.Count);
+            for (int i = 0; i < szemelyek.Count; i++)
+            {
+                if (!szemelyek[i].CvdEll)
+                {
+                    Console.WriteLine($"\tHibás az {szemelyek[i].SzemelyiSzam}szemelyi azonosíto");
+                    szemelyek.Remove(szemelyek[i]);
+                }
+
+               
+            }
+            Console.WriteLine(szemelyek.Count);*/
+
+            Console.ReadKey();
+           
         }
     }
 }
